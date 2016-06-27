ada008cc19e9491d595f656777315b720dff3666
Pakapon Kaewmark <sleep191@gmail.com>
2016-06-28 00:20:54 +0700

    Update HelloTest
    Test how Travis CI respond when there are more than one method failed

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -14,6 +14,7 @@ public class HelloTest {
 	public void testBubbleSort(){
 		assertArrayEquals(new int[]{1,2,3,4,5}, Hello.bubbleSort(new int[]{5,4,3,2,1}));
 		assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10}, Hello.bubbleSort(new int[]{10,5,9,4,7,2,6,1,8,3}));
+		fail("Test Failed");
 	}
 
 }
